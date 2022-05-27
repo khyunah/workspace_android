93d1b701bcb06eed3392c541e99286642a2a8aff
khyunah <89136556+khyunah@users.noreply.github.com>
2022-05-27 11:01:04 +0900

    탭생성, 페이저생성

--- MyTabLayout_pager/app/src/main/java/com/example/mytablayout_pager/MainActivity.java
+++ MyTabLayout_pager/app/src/main/java/com/example/mytablayout_pager/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.mytablayout_pager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout myTabLayoutContainer = findViewById(R.id.myTabLayoutContainer);
+        myTabLayoutContainer.addTab(myTabLayoutContainer.newTab().setText("shopping"));
+        myTabLayoutContainer.addTab(myTabLayoutContainer.newTab().setText("news"));
+        myTabLayoutContainer.addTab(myTabLayoutContainer.newTab().setText("webtoon"));
+
+        ViewPager myViewPagerContainer = findViewById(R.id.myViewPagerContainer);
+
+        // 어뎁터 객체를 생성
+        MyPagerAdapter myPagerAdapter = new MyPagerAdapter(getSupportFragmentManager(), 3);
+
+        // 페이지 어뎁터와 뷰페이저를 연결
+        myViewPagerContainer.setAdapter(myPagerAdapter);
+
+        // 탭이랑 페이저랑 연결하기
+
+
     }
 }
+
---
+++ MyTabLayout_pager/app/src/main/java/com/example/mytablayout_pager/MyPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.example.mytablayout_pager;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class MyPagerAdapter extends FragmentPagerAdapter {
+
+    public MyPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        switch (position){
+            case 0:
+                fragment = new FragmentShopping();
+                break;
+            case 1:
+                fragment = new FragmentTwoNews();
+                break;
+            case 2:
+                fragment = new FragmentThreeWebtoon();
+                break;
+        }
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
