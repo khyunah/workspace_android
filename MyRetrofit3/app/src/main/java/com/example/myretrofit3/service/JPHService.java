7fdf4dc5b10f0f2c1109d7b01728473a4d1f339b
khyunah <89136556+khyunah@users.noreply.github.com>
2022-05-31 10:55:13 +0900

    버튼에 따른 이벤트 핸들러 설계 ( 서버에 요청하는 )

--- MyRetrofit3/app/src/main/java/com/example/myretrofit3/service/JPHService.java
+++ MyRetrofit3/app/src/main/java/com/example/myretrofit3/service/JPHService.java
@@ -37,18 +37,18 @@ public interface JPHService {
     Call<List<ResponsePostDto>> postList();
 
     /* 4 */
-    // https://jsonplaceholder.typicode.com/posts/{postId}
+    // https://jsonplaceholder.typicode.com/posts
     // HTTP 메세지 바디 부분에 데이터를 넣어서 서버에 전달한다. ( MIME TYPE : JSON )
     // 우리의 데이터가 잘 전달 되었다면 서버측에서는 응답값을 보내준다.
     // 값을 보낼때 어떻게 데이터를 보낼지 설계 해야 한다.
-    @POST("posts/{postId}")
+    @POST("posts")
     Call<ResponsePostDto> createPost(@Body ReqPostDto reqPostDto);
 
     /* 5 */
     // https://jsonplaceholder.typicode.com/posts/{postId}
     // 수정하기
     @PUT("posts/{postId}")
-    Call<ResponsePostDto> updatePost(@Body ReqPostDto reqPostDto);
+    Call<ResponsePostDto> updatePost(@Path("postId") int postId, @Body ReqPostDto reqPostDto);
 
     /* 6 */
     // https://jsonplaceholder.typicode.com/posts/{postId}
@@ -57,8 +57,7 @@ public interface JPHService {
     Call<Void> deletePost(@Path("postId") int postId);
 
     /* 7 */
-    // https://jsonplaceholder.typicode.com/posts
-    @FormUrlEncoded
-    @GET("posts")
-    Call<List<ResponsePostDto>> searchByUserId(@Field("userId") int userId);
+    // https://jsonplaceholder.typicode.com/posts/{userId}
+    @GET("posts/{userId}")
+    Call<List<ResponsePostDto>> searchByUserId(@Path("userId") int userId);
 }
