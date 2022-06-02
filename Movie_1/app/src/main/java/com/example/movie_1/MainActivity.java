ace4112082bfaaba3819d10d292852bd898809a0
khyunah <89136556+khyunah@users.noreply.github.com>
2022-06-02 10:49:08 +0900

    애니메이션, 뷰바인딩

--- Movie_1/app/src/main/java/com/example/movie_1/MainActivity.java
+++ Movie_1/app/src/main/java/com/example/movie_1/MainActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.movie_1.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    // 뷰 바인딩 생성 방법
+    // 1. xml 파일의 컴포넌트들을 가지고만 오기
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 2. 객체 초기화 ( 메모리에 올려준다. )
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        // 3. 연결
+        setContentView(binding.getRoot());
+
     }
 }
--- Movie_1/app/src/main/java/com/example/movie_1/SplashActivity.java
+++ Movie_1/app/src/main/java/com/example/movie_1/SplashActivity.java
@@ -2,7 +2,13 @@ package com.example.movie_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +16,31 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // status 바 없애주기
+        // 앱보다 위에있는 녀석 윈도우
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        TextView textView = findViewById(R.id.splashTextView);
+
+        // 애니메이션 사용하기
+        Animation slideAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_side); // 어떤 화면에서 보일지, 어떤 화면을 보일지
+        textView.startAnimation(slideAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // getApplicationContext() : 앱 전체의 정보를 가지고 있음
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+                // SplashActivity 위에 MainActivity 가 뜰것이다. ( 화면이 )
+
+                // 시작 화면만 뜨고 내릴것이라서 종료해주기
+                finish();
+            }
+        }, 1500);
     }
 }
