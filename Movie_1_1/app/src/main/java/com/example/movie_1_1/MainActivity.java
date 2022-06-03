272842856857fad72e5af20a50206105535958db
khyunah <89136556+khyunah@users.noreply.github.com>
2022-06-03 21:02:53 +0900

    화면전화 수정 하기

--- Movie_1_1/app/src/main/java/com/example/movie_1_1/MainActivity.java
+++ Movie_1_1/app/src/main/java/com/example/movie_1_1/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.movie_1_1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.movie_1_1.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(activityMainBinding.getRoot());
+
+        Fragment movieFragment = MovieFragment.newInstance();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(activityMainBinding.mainContainer.getId(), movieFragment);
+        transaction.commit();
     }
 }
