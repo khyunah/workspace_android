2e47ef51477f9132bd842f31a58eb6ff93aebf65
khyunah <89136556+khyunah@users.noreply.github.com>
2022-06-05 11:55:06 +0900

    웹뷰로 무비사이트 띄우기

--- Movie_1_1/app/src/main/java/com/example/movie_1_1/InfoFragment.java
+++ Movie_1_1/app/src/main/java/com/example/movie_1_1/InfoFragment.java
@@ -7,10 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import com.example.movie_1_1.databinding.FragmentInfoBinding;
 
 public class InfoFragment extends Fragment {
 
     private static InfoFragment infoFragment;
+    private WebView webView;
+    private FragmentInfoBinding fragmentInfoBinding;
 
     private InfoFragment() {
 
@@ -26,13 +31,15 @@ public class InfoFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        fragmentInfoBinding = FragmentInfoBinding.inflate(getLayoutInflater());
+        webView = fragmentInfoBinding.webView;
+        webView.loadUrl("https://yts.mx/");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_info, container, false);
+        return fragmentInfoBinding.getRoot();
     }
 }
